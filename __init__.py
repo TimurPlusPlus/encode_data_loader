4f40f17d4b6bac4fe726d77b221f6d1267ad144e
timur <tim8861@yandex.com>
2018-03-14 21:47:27 +0300

    Directories hierarchy creation

--- __init__.py
+++ __init__.py
@@ -12,7 +12,10 @@ third_hierarchy_dir = 'Experiment accession'
 
 # Create a root dir
 #data_dir = sys.argv[0]
+#available_memory_volume = sys.argv[1]
+#saved_memory_volume = sys.argv[2]
 data_dir = './data'
+words_splitter = '__'
 os.makedirs(os.path.dirname(data_dir), exist_ok=True)
 
 # Filter the DataFrame
@@ -23,20 +26,26 @@ filtered_df = filtered_df.drop('Assembly', 1)
 # For the testing purpose
 test_df = filtered_df.head(10)
 filtered_df = test_df
-print(test_df.head())
-
 
 # Make folders and download files
-
 # Maybe it would be better to sort the data before iterating
 for index, row in filtered_df.iterrows():
     # Check a free space on a drive
-    new_dir = os.path.join(data_dir, row[first_hierarchy_dir])
+
+    # Directories hierarchy creation
+    new_dir = os.path.join(data_dir,
+                           row[first_hierarchy_dir].replace(' ', words_splitter),
+                           row[second_hierarchy_dir].replace(' ', words_splitter),
+                           row[third_hierarchy_dir].replace(' ', words_splitter))
+    print(new_dir)
+    os.makedirs(os.path.dirname(new_dir), exist_ok=True)
+
     # Check a file existing and md5
     # Download a file
+    # Unzip the file
 
 
-os.makedirs(os.path.dirname(new_dir), exist_ok=True)
+#
 
 
 
