8f1e07316bcefc3691cc2f76056bd9d304b9bba5
timur <tim8861@yandex.com>
2018-03-14 22:25:08 +0300

    download data md5 check

--- __init__.py
+++ __init__.py
@@ -1,6 +1,27 @@
+import hashlib
+import urllib
+import shutil
 import pandas as pd
 import os
 import sys
+
+
+def download(url, file_name):
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+
+def get_md5(file_name):
+    hash_md5 = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def is_file_changed(file_name, md5):
+    return get_md5(file_name) == md5
+
 
 metadata_file = './input_data/metadata.tsv'
 columns = ['File accession', 'Output type', 'Experiment accession', 'Biosample type', 'Derived from',
@@ -26,27 +47,26 @@ filtered_df = filtered_df.drop('Assembly', 1)
 # For the testing purpose
 test_df = filtered_df.head(10)
 filtered_df = test_df
-
-# Make folders and download files
-# Maybe it would be better to sort the data before iterating
-for index, row in filtered_df.iterrows():
-    # Check a free space on a drive
-
-    # Directories hierarchy creation
-    new_dir = os.path.join(data_dir,
-                           row[first_hierarchy_dir].replace(' ', words_splitter),
-                           row[second_hierarchy_dir].replace(' ', words_splitter),
-                           row[third_hierarchy_dir].replace(' ', words_splitter))
-    print(new_dir)
-    os.makedirs(os.path.dirname(new_dir), exist_ok=True)
-
-    # Check a file existing and md5
-    # Download a file
-    # Unzip the file
-
-
 #
 
+# Make folders and download files
+# TODO Maybe it would be better to sort the data before iterating
+for index, row in filtered_df.iterrows():
+    # TODO Check a free space on a drive
+
+    file_location = os.path.join(data_dir,
+                                 row[first_hierarchy_dir].replace(' ', words_splitter),
+                                 row[second_hierarchy_dir].replace(' ', words_splitter),
+                                 row[third_hierarchy_dir].replace(' ', words_splitter),
+                                 row['File accession'].replace(' ', words_splitter) + '.bed.gz')
+    print(file_location)
+
+    # Check a file existing and md5
+    if not os.path.isfile(file_location) or is_file_changed(file_location, row['md5sum']):
+        # TODO zip old data if md5 is not the same
+        os.makedirs(os.path.dirname(file_location), exist_ok=True)
+        download(row['File download URL'], file_location)
+        # Unzip the file
 
 
 
