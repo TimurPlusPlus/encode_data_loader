e367caac9e2685457b44d9e2399133e7f0b45fa6
timur <tim8861@yandex.com>
2018-03-17 10:41:57 +0300

    A file unzipping

--- __init__.py
+++ __init__.py
@@ -25,10 +25,21 @@ def get_md5(file_name):
 def is_file_changed(file_name, md5):
     return get_md5(file_name) == md5
 
+
 def get_uncompressed_size(file_name):
     with open(file_name, 'r') as fileobj:
         isize = gzip.read32(fileobj)  # may exceed 2GB
     return isize
+
+
+def get_device_available_space(dir_location):
+    disk = os.statvfs(dir_location)
+    return float(disk.f_bsize * disk.f_bfree)
+
+
+def gunzip(file_path, output_path):
+    with gzip.open(file_path, "rb") as f_in, open(output_path, "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
 
 # A logger configuration
 logging.basicConfig(filename='./data_loader.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
@@ -47,9 +58,12 @@ third_hierarchy_dir = 'Experiment accession'
 #data_dir = sys.argv[0]
 #available_memory_volume = sys.argv[1]
 #available_memory_volume = sys.argv[1]
-#saved_memory_volume = sys.argv[2]
+space_to_save = 20 * 1024 * 1024 * 1024  # 20GB
 data_dir = './data'
 words_splitter = '__'
+file_extension = '.bed'
+zipfile_extension = '.gz'
+
 os.makedirs(os.path.dirname(data_dir), exist_ok=True)
 logging.debug('Data dir: %s', data_dir)
 
@@ -66,23 +80,33 @@ logging.debug('First 3 dataframe rows: %s', filtered_df.head(3))
 
 # Make folders and download files
 for index, row in filtered_df.iterrows():
-    # TODO Check a free space on a drive
+    # Check a free space on a drive
+    if get_device_available_space(data_dir) < space_to_save:
+        logging.error('The device available space is over')
+        logging.info('The script work finished at %s', datetime.datetime.now())
+        exit(1)
 
     file_location = os.path.join(data_dir,
                                  row[first_hierarchy_dir].replace(' ', words_splitter),
                                  row[second_hierarchy_dir].replace(' ', words_splitter),
                                  row[third_hierarchy_dir].replace(' ', words_splitter),
-                                 row['File accession'].replace(' ', words_splitter) + '.bed.gz')
+                                 row['File accession'].replace(' ', words_splitter) + file_extension)
 
     # Check a file existing and md5
-    if not os.path.isfile(file_location) or is_file_changed(file_location, row['md5sum']):
-        logging.info('Downloading a file to %s', file_location)
-        os.makedirs(os.path.dirname(file_location), exist_ok=True)
-        download(row['File download URL'], file_location)
-        print(get_uncompressed_size(os.path.abspath(file_location)))
+    if not os.path.isfile(file_location):  # or is_file_changed(file_location, row['md5sum']):
+        zipfile_location = file_location + zipfile_extension
+        logging.info('Downloading an archive to %s', zipfile_location)
+        os.makedirs(os.path.dirname(zipfile_location), exist_ok=True)
+        download(row['File download URL'], zipfile_location)
+        logging.info('The file downloaded')
+        logging.info('Unzipping a file to %s', file_location)
         # Unzip the file
+        gunzip(zipfile_location, file_location)
+        logging.info('The file unzipped')
+        # Delete the archive
+        os.remove(zipfile_location)
 
-logging.info('Script finished at %s', datetime.datetime.now())
+logging.info('The script work finished at %s', datetime.datetime.now())
 
 
 
