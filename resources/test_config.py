513dbf2495966b35d5b094c4da637260fb1b8aec
timur <tim8861@yandex.com>
2018-03-30 21:07:27 +0300

    added test config

---
+++ resources/test_config.py
@@ -0,0 +1,18 @@
+data_dir="/home/timur/Desktop/masters/loader_test/data"                                           # A root data dir
+metadata_file="../input_data/metadata.tsv"    # Input table
+script_dir="../"
+hg_dir="/home/timur/Desktop/masters/loader_test/hg19"
+stem_loops_dir="/home/timur/Desktop/masters/loader_test/stem_loops"
+
+log_file="/home/timur/Desktop/masters/loader_test/log.log"
+
+space_to_save=1 * 1024 * 1024 * 1024                                       #1GB
+
+bedfile_extension='.bed'
+zipfile_extension='.gz'
+dirname_splitter='__'                                                       # To replace space character
+
+
+
+
+
