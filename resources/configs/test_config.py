7503089275fc1af6a44812b6efc8a9a08581813a
timur <tim8861@yandex.com>
2018-03-31 13:06:56 +0300

    changed config structure

--- resources/configs/test_config.py
+++ resources/configs/test_config.py
@@ -1,20 +1,22 @@
-data_dir="/home/timur/Desktop/masters/loader_test/data"                                           # A root data dir
-metadata_file="../input_data/metadata.tsv"    # Input table
-script_dir="../"
-hg_dir="/home/timur/Desktop/masters/loader_test/hg19/"
-chopped_hg_path=""
-stem_loops_dir="/home/timur/Desktop/masters/loader_test/stem_loops"
-data_sample_dir=data_dir # OR stem_loops_dir
-fetch_chrom_path="/home/timur/Desktop/masters/encode_data_loader/scripts/sh/fetchChromSizes.sh"
+data_dir        = "/home/timur/Desktop/masters/loader_test/data"                # Root TF dir                           # A root data dir
+metadata_file   = "../input_data/metadata.tsv"                                  # Input encode table
+script_dir      = "../"                                                         # Current script dir
+hg_dir          = "/home/timur/Desktop/masters/loader_test/hg19/"               # Hg19 dir
+chopped_hg_path = ""
+stem_loops_dir  = "/home/timur/Desktop/masters/loader_test/stem_loops"
+data_sample_dir = stem_loops_dir    # OR data_dir                               # Processed data dir
+fetch_chrom_path= "/home/timur/Desktop/masters/encode_data_loader/scripts/sh/fetchChromSizes.sh"
 
-log_file="/home/timur/Desktop/masters/loader_test/log.log"
+log_file        = "/home/timur/Desktop/masters/loader_test/log.log"
 
-space_to_save=1 * 1024 * 1024 * 1024                                       #1GB
-chop_size=1000000
+
+# OPTIONAL
+space_to_save   = 1 * 1024 * 1024 * 1024                                        #1GB
+chop_size       = 1000000
 
 bedfile_extension='.bed'
 zipfile_extension='.gz'
-dirname_splitter='__'                                                       # To replace space character
+dirname_splitter='__'                                                           # To replace space character
 
 
 
